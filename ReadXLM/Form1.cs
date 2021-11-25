20fcc14adc0937fdffa89f258ed90e22f5b77de3
Waldri Oliveira <waldrioliveira@pt.controlar.com>
2021-11-25 16:06:00 +0000

    Add project files.

---
+++ ReadXLM/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace ReadXLM
+{
+    public partial class Form1 : Form
+    {
+        public DataSet listaXml;
+        public Assembly assembObj;
+        public Form1()
+        {
+            InitializeComponent();
+            listaXml = new DataSet();
+        }
+
+        private void btnReadXML_Click(object sender, EventArgs e)
+        {
+            listaXml.ReadXml(AppDomain.CurrentDomain.BaseDirectory + "lista.xml");
+            int Tables = listaXml.Tables.Count;
+            MessageBox.Show("Tables " + Tables);
+        }
+    }
+}
